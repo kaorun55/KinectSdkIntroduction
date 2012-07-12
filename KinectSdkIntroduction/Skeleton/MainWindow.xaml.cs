7700f383e9e0ede33052e326da63bb528c12c3a8
kaorun <kaorun55@gmail.com>
2012-07-12 09:43:55 +0900

    update skeleton sample

--- KinectSdkIntroduction/Skeleton/MainWindow.xaml.cs
+++ KinectSdkIntroduction/Skeleton/MainWindow.xaml.cs
@@ -19,12 +19,16 @@ namespace SkeletonSample
             InitializeComponent();
 
             try {
-                if ( KinectSensor.KinectSensors.Count == 0 ) {
-                    throw new Exception( "Kinectが接続されていません" );
+                // 利用可能なKinectを探す
+                foreach ( var k in KinectSensor.KinectSensors ) {
+                    if ( k.Status == KinectStatus.Connected ) {
+                        kinect = k;
+                        break;
+                    }
                 }
-
-                // Kinectインスタンスを取得する
-                kinect = KinectSensor.KinectSensors[0];
+                if ( kinect == null ) {
+                    throw new Exception( "利用可能なKinectがありません" );
+                }
 
                 // すべてのフレーム更新通知をもらう
                 kinect.AllFramesReady += new EventHandler<AllFramesReadyEventArgs>( kinect_AllFramesReady );
@@ -46,35 +50,51 @@ namespace SkeletonSample
         // すべてのデータの更新通知を受け取る
         void kinect_AllFramesReady( object sender, AllFramesReadyEventArgs e )
         {
-            imageRgbCamera.Source = e.OpenColorImageFrame().ToBitmapSource();
-            imageDepthCamera.Source = e.OpenDepthImageFrame().ToBitmapSource();
+            // Disposableなのでusingでくくる
+            using ( ColorImageFrame colorFrame = e.OpenColorImageFrame() ) {
+                if ( colorFrame != null ) {
+                    imageRgbCamera.Source = colorFrame.ToBitmapSource();
+                }
+            }
 
-            // 骨格位置の表示
-            ShowSkeleton( e );
+            // Disposableなのでusingでくくる
+            using ( DepthImageFrame depthFrame = e.OpenDepthImageFrame() ) {
+                if ( depthFrame != null ) {
+                    imageDepthCamera.Source = depthFrame.ToBitmapSource();
+                }
+            }
+
+            // Disposableなのでusingでくくる
+            using ( SkeletonFrame skeletonFrame = e.OpenSkeletonFrame() ) {
+                if ( skeletonFrame != null ) {
+                    // 骨格位置の表示
+                    ShowSkeleton( skeletonFrame );
+                }
+            }
         }
 
-        private void ShowSkeleton( AllFramesReadyEventArgs e )
+        private void ShowSkeleton( SkeletonFrame skeletonFrame )
         {
             // キャンバスをクリアする
-            canvas1.Children.Clear();
+            canvasSkeleton.Children.Clear();
 
-            // スケルトンフレームを取得する
-            SkeletonFrame skeletonFrame = e.OpenSkeletonFrame();
-            if ( skeletonFrame != null ) {
-                // スケルトンデータを取得する
-                Skeleton[] skeletonData = new Skeleton[skeletonFrame.SkeletonArrayLength];
-                skeletonFrame.CopySkeletonDataTo( skeletonData );
+            // スケルトンデータを取得する
+            Skeleton[] skeletonData = new Skeleton[skeletonFrame.SkeletonArrayLength];
+            skeletonFrame.CopySkeletonDataTo( skeletonData );
 
-                // プレーヤーごとのスケルトンを描画する
-                foreach ( var skeleton in skeletonData ) {
-                    if ( skeleton.TrackingState == SkeletonTrackingState.Tracked ) {
-                        // 骨格を描画する
-                        foreach ( Joint joint in skeleton.Joints ) {
+            // プレーヤーごとのスケルトンを描画する
+            foreach ( var skeleton in skeletonData ) {
+                // 追跡されているプレイヤー
+                if ( skeleton.TrackingState == SkeletonTrackingState.Tracked ) {
+                    // 骨格を描画する
+                    foreach ( Joint joint in skeleton.Joints ) {
+                        // 追跡されている骨格
+                        if ( joint.TrackingState != JointTrackingState.NotTracked ) {
                             // 骨格の座標をカラー座標に変換する
                             ColorImagePoint point = kinect.MapSkeletonPointToColor( joint.Position, kinect.ColorStream.Format );
 
                             // 円を書く
-                            canvas1.Children.Add( new Ellipse()
+                            canvasSkeleton.Children.Add( new Ellipse()
                             {
                                 Margin = new Thickness( point.X, point.Y, 0, 0 ),
                                 Fill = new SolidColorBrush( Colors.Black ),
